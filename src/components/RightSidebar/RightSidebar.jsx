b63ce5c94f9538c0ac602cd25545a5dcd37a986c
Joao Bastos <joaobastos@outlook.de>
2025-10-20 11:38:43 +0200

    design mobile_10

--- src/components/RightSidebar/RightSidebar.jsx
+++ src/components/RightSidebar/RightSidebar.jsx
@@ -6,11 +6,13 @@ import { useContext } from "react";
 import { AppContext } from '../../context/AppContext'
 import { useEffect } from "react";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const RightSidebar = () => {
 
     const { chatUser, messages } = useContext(AppContext);
     const [msgImages, setMsgImages] = useState([]);
+    const navigate = useNavigate();
     useEffect(() => {
         let tempVar = [];
         messages.map((msg) => {
@@ -25,6 +27,8 @@ const RightSidebar = () => {
 
     return chatUser ? (
         <div className="rs">
+            <button className="button-back" onClick={() => navigate('/chat')} ><img src={assets.arrow_icon} alt="" /></button>
+            
             <div className="rs-profile">
                 <img className="profile-img" src={chatUser.userData.avatar} alt="profile pic" />
                 <div className="rs-text">
